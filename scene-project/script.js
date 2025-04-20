4f94892a2c682bd633a6ff86f2df93b5fd789576
Joe <joeshyti1@gmail.com>
2025-04-19 21:33:02 -0400

    more updates to project - new images

--- scene-project/script.js
+++ scene-project/script.js
@@ -4,25 +4,28 @@ const ctx = canvas.getContext("2d");
 const bg = new Image();
 bg.src = "images/background.jpeg";
 
-const character = new Image();
-character.src = "images/character.png";
+const hunter = new Image();
+hunter.src = "images/hunter.png";
 
-const torrent = new Image();
-torrent.src = "images/torrent.png";
+const crow = new Image();
+crow.src = "images/crow.png";
 
 bg.onload = () => {
     ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
   
     character.onload = () => {
-      ctx.drawImage(character, 100, 300, 150, 300); // adjust size/position
+      ctx.drawImage(hunter, 220, 480, 150, 330); // adjust size/position
   
-      torrent.onload = () => {
-        ctx.drawImage(torrent, 500, 350, 200, 200); // adjust size/position
+      crow.onload = () => {
+        ctx.drawImage(crow, 780, 140, 180, 180); // adjust size/position
   
         // Add text last
+        ctx.shadowColor = "black";
+        ctx.shadowBlur = 6;
         ctx.fillStyle = "white";
-        ctx.font = "30px serif";
-        ctx.fillText("Joe Shyti - Lands Between", 20, 50);
+        ctx.font = "32px Georgia";
+        ctx.fillText("Joe Shyti - Cainhurst Castle", 40, 60);
+        ctx.shadowBlur = 0;
       };
     };
   };
