02ac37dfbc8386167afc1dd9f6954c5844ac0eec
Joe <joeshyti1@gmail.com>
2025-04-19 20:45:00 -0400

    testing positions

--- scene-project/script.js
+++ scene-project/script.js
@@ -8,7 +8,7 @@ bgImg.onload = function () {
 };
 
 const charImg = new Image();
-charImg.src = "images/character.webp"; 
+charImg.src = "images/character.png"; 
 charImg.onload = function () {
   ctx.drawImage(charImg, 100, 300, 150, 300);
 };
@@ -18,3 +18,5 @@ creatureImg.src = "images/creature.png";
 creatureImg.onload = function () {
   ctx.drawImage(creatureImg, 500, 300, 200, 150);
 };
+
+const text = "Welcome to the Scene!";
