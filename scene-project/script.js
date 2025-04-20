d291e90ae98ae95fb9446cb763f55e8e1dabdff7
Joe <joeshyti1@gmail.com>
2025-04-19 20:39:45 -0400

    more updates to scene project

--- scene-project/script.js
+++ scene-project/script.js
@@ -7,14 +7,14 @@ bgImg.onload = function () {
   ctx.drawImage(bgImg, 0, 0, canvas.width, canvas.height);
 };
 
-// const charImg = new Image();
-// charImg.src = "images/character.png"; 
-// charImg.onload = function () {
-//   ctx.drawImage(charImg, 0, 0, canvas.width, canvas.height);
-// };
+const charImg = new Image();
+charImg.src = "images/character.webp"; 
+charImg.onload = function () {
+  ctx.drawImage(charImg, 100, 300, 150, 300);
+};
 
-// const creatureImg = new Image();
-// creatureImg.src = "images/creature.png";
-// creatureImg.onload = function () {
-//   ctx.drawImage(creatureImg, 0, 0, canvas.width, canvas.height);
-// };
+const creatureImg = new Image();
+creatureImg.src = "images/creature.png";
+creatureImg.onload = function () {
+  ctx.drawImage(creatureImg, 500, 300, 200, 150);
+};
